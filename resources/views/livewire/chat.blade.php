a73db6d9bf8817a2e4f7c1a3a7eb41c6ef65cad0
iCHIrTmA <jonrue.dev@gmail.com>
2024-12-01 20:34:57 +0800

    fix: force https in production

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -21,7 +21,7 @@
         <div class="bg-white rounded-full grow relative flex items-center h-10 focus-within:ring-2 ring-inset ring-blue-500">
             <input class="bg-transparent rounded-full grow px-4 py-2 text-sm h-10 focus:outline-0" placeholder="Enter case to summarize (e.g. GR. 101083 Oposa v. Factoran)" wire:model="body" />
             <button class="bg-slate-800 text-slate-50 rounded-3xl text-sm font-medium size-10 flex items-center justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4">
+                <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4">
                     <path d="M3.105 2.288a.75.75 0 0 0-.826.95l1.414 4.926A1.5 1.5 0 0 0 5.135 9.25h6.115a.75.75 0 0 1 0 1.5H5.135a1.5 1.5 0 0 0-1.442 1.086l-1.414 4.926a.75.75 0 0 0 .826.95 28.897 28.897 0 0 0 15.293-7.155.75.75 0 0 0 0-1.114A28.897 28.897 0 0 0 3.105 2.288Z" />
                 </svg>
             </button>
