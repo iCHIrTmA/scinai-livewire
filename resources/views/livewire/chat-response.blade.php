96c81291326b7ae5b72ccaa7957d7293acef163f
iCHIrTmA <jonrue.dev@gmail.com>
2024-12-03 06:19:21 +0800

    feat: markdown

--- resources/views/livewire/chat-response.blade.php
+++ resources/views/livewire/chat-response.blade.php
@@ -1,6 +1,8 @@
 <div class="w-3/4 space-y-0.5 has-[.stream:empty]:hidden">
-    <div class="text-xs">Bot</div>
+    <div class="text-xs">SCINAI</div>
     <div class="bg-slate-200 rounded-xl rounded-tl-none px-3 py-1.5 text-sm">
-        <div wire:stream="stream-{{ $this->getId() }}">{{ $response }}</div>
+        <div wire:stream="stream-{{ $this->getId() }}">
+            <x-markdown>{!! $response !!}</x-markdown>
+        </div>
     </div>
 </div>
